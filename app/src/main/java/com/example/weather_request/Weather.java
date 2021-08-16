0d3ee3b8669a8dd047d35fdae3365d2e4ce8503c
ethanbo-test <ethanbo.test@gmail.com>
2021-08-16 14:06:59 -0700

    minor change

--- app/src/main/java/com/example/weather_request/Weather.java
+++ app/src/main/java/com/example/weather_request/Weather.java
@@ -1,5 +1,20 @@
 package com.example.weather_request;
 
-public class Weather {
+import okhttp3.OkHttpClient;
 
+public class Weather {
+  private final OkHttpClient client = new OkHttpClient();
+  private String location;
+  private final String BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+  private final String APP_KEY = "appid=83f5137c3419c91f35a937d3ce9f7f54";
+
+  public Weather(String location) {
+    if (location == null) {
+      this.location = "Mountain View";
+    } else {
+      this.location = location;
+    }
+  }
+
+  
 }
